9a157f8745efcbdbd60427a666db69a2adb2ede0
Developer-Subrata <subrata.ind2@gmail.com>
2024-09-02 00:14:01 +0530

    byteStuffing complete

--- 1_bytestuffing.py
+++ 1_bytestuffing.py
@@ -1,16 +1,21 @@
 # Byte Stuffing
 sender=input("Enter Data You Want To Send : ")
 result=''
-flag='0111110'
+flag='01111110'
+esc='10000001'
 
 print("Sender Side Data Before Stuffing: ",sender)
-for i in sender:
-    if i=='F':
-        result+='EF'
-    elif i=='E':
-        result+='EE'
+i=0
+while(i<len(sender)):
+    if sender[i:i+8]==flag:
+        result+=esc+flag
+        i+=8
+    elif sender[i:i+8]==esc:
+        result+=esc+esc
+        i+=8
     else:
-        result+=i
+        result+=sender[i]
+        i+=1
 
 receiver=flag+result+flag
 print("Receiver Side Data After Stuffing: ",receiver)
