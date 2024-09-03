67182890797df169d69517f7bda46cfda4ec572b
Developer-Subrata <subrata.ind2@gmail.com>
2024-09-03 21:56:06 +0530

    byteStuffing

--- 1_bytestuffing.py
+++ 1_bytestuffing.py
@@ -1,21 +1,47 @@
-# Byte Stuffing     011111101000000110000001100000010111111001111110
-sender=input("Enter Data You Want To Send : ")
-result=''
-flag='01111110'
-esc='10000001'
+sender = input("Enter Data You Want To Send: ")
+result = ''
+flag = '01111110'
+esc = '10000001'
 
-print("Sender Side Data Before Stuffing: ",sender)
-i=0
-while(i<len(sender)):
-    if sender[i:i+8]==flag:
-        result+=esc+flag
-        i+=8
-    elif sender[i:i+8]==esc:
-        result+=esc+esc
-        i+=8
+print("Sender Side Data Before Stuffing: ", sender)
+i = 0
+while i < len(sender):
+    # Check for consecutive 'flag' or 'esc'
+    if sender[i:i+8] == flag or sender[i:i+8] == esc:
+        result += esc + sender[i:i+8]
+        i += 8
+        # Skip over any consecutive flags or esc
+        while i + 8 <= len(sender) and (sender[i:i+8] == flag or sender[i:i+8] == esc):
+            result += sender[i:i+8]
+            i += 8
     else:
-        result+=sender[i]
-        i+=1
+        result += sender[i]
+        i += 1
 
-receiver=flag+result+flag
-print("Receiver Side Data After Stuffing: ",receiver)
+receiver = flag + result + flag
+print("Receiver Side Data After Stuffing: ", receiver)
+
+
+
+
+# # Byte Stuffing     011111101000000110000001100000010111111001111110
+# sender=input("Enter Data You Want To Send : ")
+# result=''
+# flag='01111110'
+# esc='10000001'
+
+# print("Sender Side Data Before Stuffing: ",sender)
+# i=0
+# while(i<len(sender)):
+#     if sender[i:i+8]==flag:
+#         result+=esc+flag
+#         i+=8
+#     elif sender[i:i+8]==esc:
+#         result+=esc+esc
+#         i+=8
+#     else:
+#         result+=sender[i]
+#         i+=1
+
+# receiver=flag+result+flag
+# print("Receiver Side Data After Stuffing: ",receiver)
--- 2_bytedestuffing.py
+++ 2_bytedestuffing.py
@@ -1,21 +1,45 @@
-# Byte Stuffing     011111101000000110000001100000010111111001111110
-sender1=input("Enter Data You Want To Send : ")
-result=''
-flag='01111110'
-esc='10000001'
-sender=sender1[8:-8]
+# # Byte Stuffing     011111101000000110000001100000010111111001111110
+# sender1=input("Enter Data You Want To Send : ")
+# result=''
+# flag='01111110'
+# esc='10000001'
+# sender=sender1[8:-8]
 
-print("Sender Side Data Before De-Stuffing: ",sender1)
-i=0
-while(i<len(sender)):
-    if sender[i:i+16]==esc+esc:
-        result+=esc
-        i+=16
-    elif sender[i:i+16]==esc+flag:
-        result+=flag
-        i+=16
+# print("Sender Side Data Before De-Stuffing: ",sender1)
+# i=0
+# while(i<len(sender)):
+#     if sender[i:i+16]==esc+esc:
+#         result+=esc
+#         i+=16
+#     elif sender[i:i+16]==esc+flag:
+#         result+=flag
+#         i+=16
+#     else:
+#         result+=sender[i]
+#         i+=1
+
+# print("Receiver Side Data After De-Stuffing: ",result)
+sender1 = input("Enter Data You Want To Send: ")
+result = ''
+flag = '01111110'
+esc = '10000001'
+sender = sender1[8:-8]  # Removing the starting and ending flag
+
+print("Sender Side Data Before De-Stuffing: ", sender1)
+i = 0
+while i < len(sender):
+    if sender[i:i+8] == esc:
+        # If 'esc' is found, check the next 8 bits
+        next_seq = sender[i+8:i+16]
+        
+        if next_seq == flag or next_seq == esc:
+            result += next_seq  # Add the next sequence (flag or esc)
+            i += 16  # Skip both the esc and the next sequence
+        else:
+            result += sender[i:i+8]  # Add the current esc (if it's not followed by flag or esc)
+            i += 8
     else:
-        result+=sender[i]
-        i+=1
+        result += sender[i]
+        i += 1
 
-print("Receiver Side Data After De-Stuffing: ",result)
+print("Receiver Side Data After De-Stuffing: ", result)
